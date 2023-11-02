e843d1ab278dad9deaf09d8209585ad95d7181b5
Midland707 <rus14job@ukr.net>
2023-11-02 23:52:45 +0200

    update getAllIrders

--- controllers/orders/getAllOrders.js
+++ controllers/orders/getAllOrders.js
@@ -1,9 +1,46 @@
+// const {
+//   order: { Order },
+// } = require('../../models');
+
+// module.exports = async (_, res) => {
+//   const orders = await Order.find();
+
+//   res.json({ orders, totalCount: orders.length });
+// };
+
 const {
   order: { Order },
 } = require('../../models');
 
-module.exports = async (_, res) => {
-  const orders = await Order.find();
+module.exports = async (req, res) => {
+  let { query = '', limit } = req.query;
+
+  limit = limit || 10;
+
+  let orders = [];
+  let result = [];
+
+  if (query) {
+    const response = await Order.aggregate([
+      {
+        $addFields: {
+          totalPriceStr: { $toString: '$totalPrice' },
+          orderId: { $toString: '$_id' },
+        },
+      },
+    ]).limit(limit);
+    result = response.filter(el => {
+      return (
+        el.phone.includes(query) ||
+        el.orderId.includes(query) ||
+        el.totalPriceStr.includes(query)
+      );
+    });
+
+    orders.push(...(result ?? []));
+  } else {
+    orders = await Order.find().limit(limit);
+  }
 
   res.json({ orders, totalCount: orders.length });
 };
