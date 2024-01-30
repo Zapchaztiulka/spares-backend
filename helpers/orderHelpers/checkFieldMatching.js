7f08b4a89393871a1970f66fb0fdbea1a942ea32
Oleh Kliapko <goldclub1977@gmail.com>
2024-01-30 13:40:25 +0200

    Update checkFieldMatching.js

--- helpers/orderHelpers/checkFieldMatching.js
+++ helpers/orderHelpers/checkFieldMatching.js
@@ -44,47 +44,54 @@ module.exports = async (additionalData, order) => {
   }
 
   if (order) {
-    if (legalEntityData !== 'undefined') {
-      if (
-        (userType &&
-          userType === patterns.userTypes[0] &&
-          legalEntityData !== null) ||
-        (!userType === 'undefined' && order.userType === patterns.userTypes[0])
-      ) {
-        throw HttpError(400, 'Legal entity data is prohibited for individuals');
-      }
-
-      if (
-        !userType === 'undefined' &&
-        order.userType === patterns.userTypes[0] &&
-        legalEntityData !== null
-      ) {
-        throw HttpError(400, 'Legal entity data is prohibited for individuals');
-      }
-
-      if (
-        userType &&
-        userType !== patterns.userTypes[0] &&
-        legalEntityData === null
-      ) {
-        throw HttpError(
-          400,
-          'Legal entity data for company or entrepreneur can not be "null"',
-        );
-      }
+    if (
+      (userType !== undefined &&
+        userType === patterns.userTypes[0] &&
+        legalEntityData !== undefined &&
+        legalEntityData !== null) ||
+      (legalEntityData === undefined && order.legalEntityData !== null)
+    ) {
+      throw HttpError(
+        400,
+        `"userType" can NOT be ${userType} because "legalEntityData" is filled. Change "userType" or the value assigned to "legalEntityData" is "null"`,
+      );
     }
 
-    if (userType && legalEntityData === 'undefined') {
-      if (userType !== patterns.userTypes[0] && !order.legalEntityData) {
-        throw HttpError(400, 'Legal entity data are required');
-      }
+    if (
+      legalEntityData !== undefined &&
+      legalEntityData !== null &&
+      ((userType !== undefined && userType === patterns.userTypes[0]) ||
+        (userType === undefined && order.userType === patterns.userTypes[0]))
+    ) {
+      throw HttpError(
+        400,
+        `"legalEntityData" can NOT be filled because "userType" is ${patterns.userTypes[0]}. The value assigned to "legalEntityData" is "null" or change "userType"`,
+      );
+    }
 
-      if (userType === patterns.userTypes[0] && order.legalEntityData) {
-        throw HttpError(
-          400,
-          'Legal entity data exists in database and is prohibited for individuals. Change legalEntityData to "null"',
-        );
-      }
+    if (
+      (userType !== undefined &&
+        userType !== patterns.userTypes[0] &&
+        legalEntityData !== undefined &&
+        legalEntityData === null) ||
+      (legalEntityData === undefined && order.legalEntityData === null)
+    ) {
+      throw HttpError(
+        400,
+        `"userType" can NOT be ${userType} because "legalEntityData" is null. Change "userType" or fill "legalEntityData" fields`,
+      );
+    }
+
+    if (
+      legalEntityData !== undefined &&
+      legalEntityData === null &&
+      ((userType !== undefined && userType !== patterns.userTypes[0]) ||
+        (userType === undefined && order.userType !== patterns.userTypes[0]))
+    ) {
+      throw HttpError(
+        400,
+        `"legalEntityData" can NOT be "null" because "userType" is NOT ${patterns.userTypes[0]}. Fill "legalEntityData" fields or change "userType"`,
+      );
     }
 
     if (deliveryMethodId) {
