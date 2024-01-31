bb3090e1c1f96cd4c37c51c775e7930128093daf
Oleh Kliapko <goldclub1977@gmail.com>
2024-01-31 11:29:32 +0200

    Update checkFieldMatching.js

--- helpers/orderHelpers/checkFieldMatching.js
+++ helpers/orderHelpers/checkFieldMatching.js
@@ -43,95 +43,76 @@ module.exports = async (additionalData, order) => {
     }
   }
 
-  if (order) {
-    if (
-      (userType !== undefined &&
-        userType === patterns.userTypes[0] &&
-        legalEntityData !== undefined &&
-        legalEntityData !== null) ||
-      (legalEntityData === undefined && order.legalEntityData !== null)
-    ) {
+  if (order && deliveryMethodId) {
+    const { isByCourier } = patterns.deliveryMethods.find(
+      method => method.deliveryMethodId === deliveryMethodId,
+    );
+
+    if ((isByCourier && !deliveryCity) || (isByCourier && !deliveryAddress)) {
       throw HttpError(
         400,
-        `"userType" can NOT be ${userType} because "legalEntityData" is filled. Change "userType" or the value assigned to "legalEntityData" is "null"`,
+        'deliveryCity and deliveryAddress are required for delivery by courier',
       );
     }
 
-    if (
-      legalEntityData !== undefined &&
-      legalEntityData !== null &&
-      ((userType !== undefined && userType === patterns.userTypes[0]) ||
-        (userType === undefined && order.userType === patterns.userTypes[0]))
-    ) {
+    if (!isByCourier && !deliveryOffice) {
       throw HttpError(
         400,
-        `"legalEntityData" can NOT be filled because "userType" is ${patterns.userTypes[0]}. The value assigned to "legalEntityData" is "null" or change "userType"`,
+        'deliveryOffice are required when delivering WITHOUT a courier',
+      );
+    }
+  }
+
+  if (
+    order &&
+    !deliveryMethodId &&
+    (deliveryRegion === '' ||
+      deliveryDistrict === '' ||
+      deliveryCity === '' ||
+      deliveryAddress === '' ||
+      deliveryOffice === '' ||
+      deliveryRate === '' ||
+      deliveryRegion ||
+      deliveryDistrict ||
+      deliveryCity ||
+      deliveryAddress ||
+      deliveryOffice ||
+      deliveryRate)
+  ) {
+    throw HttpError(
+      400,
+      'any delivery fields MUST NOT be without a deliveryMethodId',
+    );
+  }
+
+  if (order && userType && legalEntityData === undefined) {
+    throw HttpError(
+      400,
+      'when updating the "userType" field, the "legalEntityData" field must be updated too',
+    );
+  }
+
+  const isLegalEntityData = !!(legalEntityData || legalEntityData === null);
+
+  if (order && userType === undefined && isLegalEntityData) {
+    throw HttpError(
+      400,
+      'when updating the "legalEntityData" field, the "userType" field must be updated too',
+    );
+  }
+
+  if (order && userType && isLegalEntityData) {
+    if (userType === patterns.userTypes[0] && legalEntityData !== null) {
+      throw HttpError(
+        400,
+        `when "userType" field is ${userType}, the "legalEntityData" field must be "null"`,
       );
     }
 
-    if (
-      (userType !== undefined &&
-        userType !== patterns.userTypes[0] &&
-        legalEntityData !== undefined &&
-        legalEntityData === null) ||
-      (legalEntityData === undefined && order.legalEntityData === null)
-    ) {
+    if (userType !== patterns.userTypes[0] && legalEntityData === null) {
       throw HttpError(
         400,
-        `"userType" can NOT be ${userType} because "legalEntityData" is null. Change "userType" or fill "legalEntityData" fields`,
-      );
-    }
-
-    if (
-      legalEntityData !== undefined &&
-      legalEntityData === null &&
-      ((userType !== undefined && userType !== patterns.userTypes[0]) ||
-        (userType === undefined && order.userType !== patterns.userTypes[0]))
-    ) {
-      throw HttpError(
-        400,
-        `"legalEntityData" can NOT be "null" because "userType" is NOT ${patterns.userTypes[0]}. Fill "legalEntityData" fields or change "userType"`,
-      );
-    }
-
-    if (deliveryMethodId) {
-      const { isByCourier } = patterns.deliveryMethods.find(
-        method => method.deliveryMethodId === deliveryMethodId,
-      );
-
-      if (
-        isByCourier &&
-        (!deliveryCity ||
-          !deliveryAddress ||
-          !order.deliveryCity ||
-          !order.deliveryAddress)
-      ) {
-        throw HttpError(
-          400,
-          'deliveryCity and deliveryAddress are required for delivery by courier',
-        );
-      }
-
-      if (!isByCourier && (!deliveryOffice || order.deliveryOffice)) {
-        throw HttpError(
-          400,
-          'deliveryOffice are required when delivering WITHOUT a courier',
-        );
-      }
-    }
-
-    if (
-      !deliveryMethodId &&
-      (deliveryRegion ||
-        deliveryDistrict ||
-        deliveryCity ||
-        deliveryAddress ||
-        deliveryOffice ||
-        deliveryRate)
-    ) {
-      throw HttpError(
-        400,
-        'any delivery fields MUST NOT be without a deliveryMethodId',
+        `when "userType" field is ${userType}, the "legalEntityData" fields must be filled`,
       );
     }
   }
